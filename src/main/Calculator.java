53d5d91703f3128893000e7266435da06a08a348
MFAthesecond <141055098+MFAthesecond@users.noreply.github.com>
2024-01-06 05:30:24 +0300

    Add files via upload

---
+++ src/main/Calculator.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class Calculator {
+    public Calculator() {
+    }
+
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int substract(int a,int b){
+        return a-b;
+    }
+}
